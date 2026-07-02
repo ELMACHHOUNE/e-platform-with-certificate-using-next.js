dd846b5af244c4ff03b78e34f3aff874695117a3
ELMACHHOUNE <mohamed20rida@gmail.com>
2026-07-02 22:02:28 +0100

    feat: add Admin model export and configure serverExternalPackages

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -1,3 +1,4 @@
 export { Student, type IStudent } from "./Student";
 export { Course, type ICourse } from "./Course";
 export { Instructor, type IInstructor } from "./Instructor";
+export { Admin, type IAdmin } from "./Admin";
